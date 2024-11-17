11ba36c37c33fde6037dbd4e13fd5e9ad3c7e267
Matheus Nagao <matheusshimoda325@gmail.com>
2024-11-17 13:24:01 -0300

    Adição de estilo ao gráfico!

--- src/js/grafico.js
+++ src/js/grafico.js
@@ -3,18 +3,33 @@ const ctx = document.getElementById('line-chart');
 new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ['Meta', 2024, 2030, 2050],
+    labels: ['Meta', '2024 (jan-set)', '2030', '2050'],
     datasets: [{
-      label: 'Temperatura média global acima da média pré-industrial',
+      label: 'Temperatura média global acima da média pré-industrial em °C ',
       data: [2, 1.54, 2, 3,5 ],
-      borderWidth: 1
+      borderWidth: 1,
+      backgroundColor: '#E3FF47',
+      borderColor: '#E3FF47',
+      pointHoverBackgroundColor: '#FFFFFF',
+
+    
+
     }]
   },
   options: {
     scales: {
+      x: {
+        grid: {
+            color: 'grey'
+        }
+      },
+
       y: {
-        beginAtZero: true
+        beginAtZero: true,
+        grid: {
+            color: 'grey'
+        }
       }
-    }
+    },
   }
 });
