a5eeafa091dece9639e7dce64ebbf01b17acf85d
Matheus Nagao <matheusshimoda325@gmail.com>
2024-11-17 15:24:55 -0300

    Algumas alterações no posicionamento do gráfico e adição de textos explicativos!

--- src/js/grafico.js
+++ src/js/grafico.js
@@ -3,10 +3,10 @@ const ctx = document.getElementById('line-chart');
 new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ['Meta', '2024 (jan-set)', '2030', '2050'],
+    labels: ['Limite', '2024 (jan-set)', '2030', '2050'],
     datasets: [{
       label: 'Temperatura média global acima da média pré-industrial em °C ',
-      data: [2, 1.54, 2, 3,5 ],
+      data: [2.0, 1.54, 2.0, 3.56 ],
       borderWidth: 1,
       backgroundColor: '#E3FF47',
       borderColor: '#E3FF47',
